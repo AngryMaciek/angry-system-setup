ef18c351582394272ab3939b228cdda815719403
Maciej Bak <wsciekly.maciek@gmail.com>
2020-04-16 02:31:48 +0200

    add template for ubuntu installation script

---
+++ install-ubuntu.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+###############################################################################
+#
+#   [TITLE, DESCRIPTION]
+#
+#   AUTHOR: Maciej_Bak
+#   AFFILIATION: Swiss_Institute_of_Bioinformatics
+#   CONTACT: wsciekly.maciek@gmail.com
+#   CREATED: 16-04-2020
+#   LICENSE: MIT
+#   USAGE: bash install-ubuntu.sh
+#
+###############################################################################
+
+cd
+
+#remove unnecessary soft
+
+#update and upgrade apt
+
+#install c c++ fortran compilers <?>
+
+#install miniconda
+
+# backup old git config and bashrc
+
+# make symlinks to gitconfig dotfile
+
+#clone my repos
+#textfile template
+#cookiecutters
+#conda-envs
+#custom bash
+#sc4da
+#custom pylintrc
+
+#shellckech and lint this script at the end!
