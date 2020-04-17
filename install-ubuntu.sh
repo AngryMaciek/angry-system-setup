f933a7aa539a9734a09f4c97fd1d284b95e30ead
Maciek <wsciekly.maciek@gmail.com>
2020-04-17 16:33:01 +0200

    update ubuntu install

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -16,7 +16,7 @@
 cd $HOME
 
 # backup old configs
-mkdir backup || 1 "Dicectory 'backup' already exists."
+mkdir backup || echo "Dicectory 'backup' already exists."
 
 
 # install my bash configuration
