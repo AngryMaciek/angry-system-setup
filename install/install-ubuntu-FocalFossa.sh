f8a7486e7b3d57d160db827ec54f6c8d28ac6889
Maciek <wsciekly.maciek@gmail.com>
2020-04-24 15:01:57 +0200

    correct update

--- install/install-ubuntu-FocalFossa.sh
+++ install/install-ubuntu-FocalFossa.sh
@@ -147,7 +147,7 @@ echo $(date)
 echo "Installing important software"
 sudo snap install code --classic
 code --version
-sudoguake --version
+sudo guake --version
 sudo apt-get install htop --yes
 htop --version
 sudo apt-get install terminator --yes
