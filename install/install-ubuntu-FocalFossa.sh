d3fd20debb0bc28d3d2b8c5939a851b977642880
Maciek <wsciekly.maciek@gmail.com>
2020-05-08 15:41:45 +0200

    clean structure

--- install/install-ubuntu-FocalFossa.sh
+++ install/install-ubuntu-FocalFossa.sh
@@ -9,7 +9,7 @@
 #   CONTACT: wsciekly.maciek@gmail.com
 #   CREATED: 16-04-2020
 #   LICENSE: MIT
-#   USAGE: sudo bash install-ubuntu.sh
+#   USAGE: sudo bash install-ubuntu-FocalFossa.sh
 #
 ###############################################################################
 
@@ -73,7 +73,7 @@ cleanup () {
     rm -rf conda-envs
     rm -rf SC4DA
     # clean vim and prezto stuff
-    cd "$user_dir"
+    cd "$USER_DIR"
     echo "Installation aborted!"
     echo "Exit status: $rc"
     echo $SEP
@@ -86,8 +86,8 @@ trap cleanup EXIT SIGINT # move this after Backup?
 set -euo pipefail
 
 # remember paths
-user_dir=$PWD
-install_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+USER_DIR=$PWD
+INSTALL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # work in $SUDO_USER home directory
 USER_HOME=$(sudo -u $SUDO_USER -H -s eval 'echo $HOME')
@@ -113,6 +113,7 @@ if [[ -d "Backup" ]]
         exit 1
 fi
 mkdir Backup
+chattr +i Backup
 cp .bashrc Backup/.bashrc # .bashrc is always present
 if [[ -f .gitconfig ]]
     then
@@ -127,8 +128,8 @@ echo $SEP
 # copy the dotflies
 echo $(date)
 echo "Copying configuration files"
-sudo -u $SUDO_USER cp $install_dir/../dotfiles/.gitconfig .gitconfig
-sudo -u $SUDO_USER cp $install_dir/../dotfiles/.pylintrc .pylintrc
+sudo -u $SUDO_USER cp $INSTALL_DIR/../dotfiles/.gitconfig .gitconfig
+sudo -u $SUDO_USER cp $INSTALL_DIR/../dotfiles/.pylintrc .pylintrc
 echo $SEP
 
 # install my bash configuration
@@ -166,6 +167,12 @@ apt-get install gfortran -qq
 gfortran --version
 echo $SEP
 
+# Install GNOME Flashback desktop environment
+echo $(date)
+echo "Installing GNOME Flashback"
+apt-get install gnome-session-flashback -qq
+echo $SEP
+
 # install important software:
 echo $(date)
 echo "Installing important software"
@@ -174,7 +181,8 @@ hash gparted
 apt-get install zsh -qq
 zsh --version
 snap install code --classic
-sudo -u $SUDO_USER code --version
+#sudo -u $SUDO_USER code --version
+code --version
 apt-get install guake -qq
 guake --version
 apt-get install htop -qq
@@ -185,10 +193,12 @@ snap install tmux --classic
 hash tmux
 apt-get install sshfs -qq
 sshfs --version
-sudo -u $SUDO_USER wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo -u $SUDO_USER wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install ./google-chrome-stable_current_amd64.deb -qq
 google-chrome --version
-sudo -u $SUDO_USER rm -f google-chrome-stable_current_amd64.deb
+rm -f google-chrome-stable_current_amd64.deb
+#sudo -u $SUDO_USER rm -f google-chrome-stable_current_amd64.deb
 apt-get install vim -qq
 vim --version
 snap install slack --classic
@@ -198,8 +208,9 @@ hash bitwarden
 snap install gimp
 gimp --version
 snap install inkscape
-sudo -u $SUDO_USER inkscape --version
-# install Adobe Reader
+#sudo -u $SUDO_USER inkscape --version
+inkscape --version
+# install Adobe Reader:
 apt-get install \
 gdebi-core \
 libxml2:i386 \
@@ -207,23 +218,22 @@ libcanberra-gtk-module:i386 \
 gtk2-engines-murrine:i386 \
 libatk-adaptor:i386 \
 -qq
-sudo -u $SUDO_USER wget ftp://ftp.adobe.com/pub/adobe/reader/unix/9.x/9.5.5/enu/AdbeRdr9.5.5-1_i386linux_enu.deb
+#sudo -u $SUDO_USER wget ftp://ftp.adobe.com/pub/adobe/reader/unix/9.x/9.5.5/enu/AdbeRdr9.5.5-1_i386linux_enu.deb
+wget ftp://ftp.adobe.com/pub/adobe/reader/unix/9.x/9.5.5/enu/AdbeRdr9.5.5-1_i386linux_enu.deb
 yes | gdebi AdbeRdr9.5.5-1_i386linux_enu.deb & # workaround for this installer
 sleep 180
-sudo -u $SUDO_USER rm -rf AdbeRdr9.5.5-1_i386linux_enu.deb
+#sudo -u $SUDO_USER rm -rf AdbeRdr9.5.5-1_i386linux_enu.deb
+rm -rf AdbeRdr9.5.5-1_i386linux_enu.deb
 acroread -version
 echo $SEP
-
-
 
 # remove unnecessary software
 echo $(date)
 echo "Uninstalling unnecessary software"
 apt-get purge firefox* -qq
+apt-get clean
+apt-get autoremove -qq
 echo $SEP
-#sudo apt-get clean
-#sudo apt-get autoremove
-
 
 # Clone Vim Configuration
 # https://github.com/AngryMaciek/custom_vim
@@ -258,7 +268,7 @@ sudo -u $SUDO_USER echo $EXPORT_TEMPLATES >> custom_bash/bashrc.local
 sudo -u $SUDO_USER chmod +x textfile-templates/template
 echo $SEP
 
-# install my cookiecutters
+# clone my cookiecutters
 # https://github.com/AngryMaciek/cookiecutters
 echo $(date)
 echo "Cloning cookiecutters"
@@ -268,10 +278,12 @@ echo $SEP
 # download and install Miniconda3
 echo $(date)
 echo "Installing Miniconda3"
-sudo -u $SUDO_USER wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+#sudo -u $SUDO_USER wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sudo -u $SUDO_USER bash Miniconda3-latest-Linux-x86_64.sh -b -p miniconda3
-sudo -u $SUDO_USER cat $install_dir/conda-init.txt >> .bashrc
-sudo -u $SUDO_USER rm -f Miniconda3-latest-Linux-x86_64.sh
+sudo -u $SUDO_USER cat $INSTALL_DIR/conda-init.txt >> .bashrc
+#sudo -u $SUDO_USER rm -f Miniconda3-latest-Linux-x86_64.sh
+rm -f Miniconda3-latest-Linux-x86_64.sh
 echo $SEP
 
 # install my conda env recipes
@@ -321,32 +333,25 @@ echo $SEP
 echo $(date)
 echo "Removing unused packages and cache from conda"
 #sudo -u $SUDO_USER conda clean --all --yes
-sudo -i -u $SUDO_USER bash -i conda-clean.sh # -i?
+sudo -i -u $SUDO_USER bash -i conda-clean.sh
 echo $SEP
 
 # set a wallpaper
-# https://askubuntu.com/questions/66914/how-to-change-desktop-background-from-command-line-in-unity
 echo $(date)
 echo "Setting a wallpaper"
 #RESOLUTION=$(xdpyinfo | awk '/dimensions/{print $2}')
 sudo -u $SUDO_USER gsettings set org.gnome.desktop.background picture-uri \
-file://$USER_HOME/system-setup/ubuntu-wallpaper-3840x2160.jpg
+file://$INSTALL_DIR/../ubuntu-wallpaper-3840x2160.jpg
 echo $SEP
 
-# Install GNOME Flashback desktop environment
-echo $(date)
-echo "Installing GNOME Flashback"
-apt-get install gnome-session-flashback -qq
-echo $SEP
-
-duration=$SECONDS
-duration_h=$(($duration / 3600))
-duration_m=$((($duration / 60) % 60))
-duration_s=$(($duration % 60))
+DURATION=$SECONDS
+DURATION_H=$(($DURATION / 3600))
+DURATION_M=$((($DURATION / 60) % 60))
+DURATION_S=$(($DURATION % 60))
 
 # finish with rebooting the system
 echo $(date)
-echo "Setup time: "$duration_h"h"$duration_m"m"$duration_s"s"
+echo "Setup time: "$DURATION_H"h"$DURATION_M"m"$DURATION_S"s"
 echo "Setup completed successfully!"
 echo "System will reboot in 60s"
 echo $SEP
@@ -358,14 +363,12 @@ sleep 60
 #
 # Future releases: 
 #
-# * gnome flashback as default desktop, clean icons and panels (gnome dotfile?)
+# * auto switch to gnome flashback, clean icons and panels (dotfile?)
 # * vs code config - dotfiles? plugins?
 # * .guake
 # * .terminator
 #
 ###############################################################################
-
-# reorder install: gnome on top? update, purge, upgrade, intall?
 
 # test while clonning to different location and calling from another one!
 
