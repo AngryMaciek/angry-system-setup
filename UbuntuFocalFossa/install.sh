f110ea8ff10087607d8f81fa45309ee6052e73a0
Maciek Bąk <wsciekly.maciek@gmail.com>
2020-06-04 13:37:54 +0200

    correct gnome-panel commands

--- UbuntuFocalFossa/install.sh
+++ UbuntuFocalFossa/install.sh
@@ -394,8 +394,8 @@ sudo -u "$SUDO_USER" gsettings set org.gnome.nautilus.list-view default-zoom-lev
 sudo -u "$SUDO_USER" gsettings set org.gnome.gnome-flashback.desktop.icons show-home false
 sudo -u "$SUDO_USER" gsettings set org.gnome.gnome-flashback.desktop.icons show-trash false
 cp "$INSTALL_DIR"/maciek-gnome-flashback.layout /usr/share/gnome-panel/layouts/maciek-gnome-flashback.layout
-dconf reset -f /org/gnome/gnome-panel/
-gsettings set org.gnome.gnome-panel.general default-layout "maciek-gnome-flashback"
+sudo -u "$SUDO_USER" dconf reset -f /org/gnome/gnome-panel/
+sudo -u "$SUDO_USER" gsettings set org.gnome.gnome-panel.general default-layout "maciek-gnome-flashback"
 echo $SEP
 
 DURATION=$SECONDS
