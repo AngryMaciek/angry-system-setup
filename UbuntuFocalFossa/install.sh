d6019a1fca57037d5e1f7f5e6b1a441a650370cb
Maciek Bąk <wsciekly.maciek@gmail.com>
2020-06-06 22:56:45 +0200

    comment dotfiles part

--- UbuntuFocalFossa/install.sh
+++ UbuntuFocalFossa/install.sh
@@ -258,8 +258,8 @@ echo $SEP
 date
 echo "Cloning configuration files"
 sudo -u "$SUDO_USER" git clone https://github.com/AngryMaciek/small-dotfiles.git
-sudo -u "$SUDO_USER" stow -vSt $USER_HOME small-dotfiles/dotfiles/git
-sudo -u "$SUDO_USER" stow -vSt $USER_HOME small-dotfiles/dotfiles/pylint
+#sudo -u "$SUDO_USER" stow -vSt $USER_HOME small-dotfiles/dotfiles/git
+#sudo -u "$SUDO_USER" stow -vSt $USER_HOME small-dotfiles/dotfiles/pylint
 echo $SEP
 
 # install my textfile templates
